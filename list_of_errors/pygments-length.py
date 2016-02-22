f0bce2014f67510fa7b38cd5ed13a3fa571bc6a1
Yijun <yijun.z95@gmail.com>
2016-02-21 16:02:53 -0800

    added number of tokens CDF

--- list_of_errors/pygments-length.py
+++ list_of_errors/pygments-length.py
@@ -23,5 +23,51 @@ with open(target) as inf:
           num += 1
       summary.append(num)
 
-for i in summary:
-  print(i)
+print(sum(i <= 10 for i in summary))
+print(sum(i <= 20 for i in summary))
+print(sum(i <= 30 for i in summary))
+print(sum(i <= 40 for i in summary))
+print(sum(i <= 50 for i in summary))
+print(sum(i <= 60 for i in summary))
+print(sum(i <= 70 for i in summary))
+print(sum(i <= 80 for i in summary))
+print(sum(i <= 90 for i in summary))
+print(sum(i <= 100 for i in summary))
+print(sum(i <= 110 for i in summary))
+print(sum(i <= 120 for i in summary))
+print(sum(i <= 130 for i in summary))
+print(sum(i <= 140 for i in summary))
+print(sum(i <= 150 for i in summary))
+print(sum(i <= 160 for i in summary))
+print(sum(i <= 170 for i in summary))
+print(sum(i <= 180 for i in summary))
+print(sum(i <= 190 for i in summary))
+print(sum(i <= 200 for i in summary))
+print(sum(i <= 210 for i in summary))
+print(sum(i <= 220 for i in summary))
+print(sum(i <= 230 for i in summary))
+print(sum(i <= 240 for i in summary))
+print(sum(i <= 250 for i in summary))
+print(sum(i <= 260 for i in summary))
+print(sum(i <= 270 for i in summary))
+print(sum(i <= 280 for i in summary))
+print(sum(i <= 290 for i in summary))
+print(sum(i <= 300 for i in summary))
+print(sum(i <= 310 for i in summary))
+print(sum(i <= 320 for i in summary))
+print(sum(i <= 330 for i in summary))
+print(sum(i <= 340 for i in summary))
+print(sum(i <= 350 for i in summary))
+print(sum(i <= 360 for i in summary))
+print(sum(i <= 370 for i in summary))
+print(sum(i <= 380 for i in summary))
+print(sum(i <= 390 for i in summary))
+print(sum(i <= 400 for i in summary))
+
+
+
+# 7066 'bad' programs
+#print(len(summary))
+
+# average 47.9 tokens
+#print(sum(summary) * 1.0/len(summary))
