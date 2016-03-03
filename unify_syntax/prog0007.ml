1a2417dd6c1be20c183ce1390d34903c3ad9423a
Soomin Lee <slee0122@gmail.com>
2016-03-02 17:57:14 -0800

    Added files via upload

--- unify_syntax/prog0007.ml
+++ unify_syntax/prog0007.ml
@@ -1,12 +1,8 @@
 let _= additivePersistence 987623444;
 
-(* uncomment and run AFTER you have implemented additivePersistence  
 
-let _ = additivePersistence 9876
 
-*)
 
-(* NOTE: assume that digitalRoot is only called with positive numbers *)
 let rec sum n = 
 if n <= 0 
 then 0
