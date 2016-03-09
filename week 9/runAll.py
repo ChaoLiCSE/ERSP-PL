d28c60d010d85b2381b7172288cd5cde5a337c3a
cs30xbi <cs30xbi@ieng6.ucsd.edu>
2016-03-09 10:22:15 -0800

    optimize the program to be more efficient, need debug on spanSize and print

--- week 9/runAll.py
+++ week 9/runAll.py
@@ -1,6 +1,24 @@
 import os
 import json
 import min_interval
+import random
+
+def runOnRandomPrograms(fun, directory,num, prog_num):
+  j=0
+  while(j<num):
+    i = random.choice(os.listdir(directory))
+
+    #print("num is")
+    #print(j)
+
+    if not i.lower().endswith( '.ml'):
+      continue
+
+    #print("prog is")
+    #print(i)
+    j=j+1
+    yield fun(directory, i)
+
 
 def runOnAllPrograms(fun, directory):
   for i in os.listdir(directory):
@@ -12,12 +30,18 @@ def runOnAllPrograms(fun, directory):
 ######################################################## Main
 dir = os.path.abspath(__file__ + '/../../')
 target = os.path.join(dir, 'unify_syntax')
+target2 = os.path.join(__file__ + '/../../../yunounderstand/data/sp14/prog/unify')
 summary = os.path.join(target, 'dictionary.json')
 pretty_print = os.path.join(target, 'summary.txt')
 
-result = runOnAllPrograms(min_interval.find_min_interval, target)
+result = runOnRandomPrograms(min_interval.find_min_interval, target2, 1,4874)
+print("this is result")
+print (result)
+
 with open(summary, 'w') as of:
   for i in result:
+    print("this is i")
+    print (i)
     json.dump(i, of)
     of.write('\n')
   of.close()
@@ -31,7 +55,7 @@ with open(summary, 'r') as inf, open(pretty_print, 'w') as of2:
     of2.write('file no.: ' + str(index) + '\n')
 
     of2.write('original code: \n' + item['in'] + ' \n')
-    of2.write('fixed code: \n' + item['fixed'] + ' \n')
+    of2.write('fixed code: \n' + str(item['fixed']) + ' \n')
     of2.write('span size: \n' + str(item['span-size']) + ' \n')
     of2.write('span fraction: \n' + str(item['span-fraction']) + ' \n')
 
