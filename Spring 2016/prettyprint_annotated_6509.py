b6aad083aaf54ad57b9992b837e2c676813bffde
Yijun <yijun.z95@gmail.com>
2016-04-23 21:32:47 -0700

    print annotated_6509

---
+++ Spring 2016/prettyprint_annotated_6509.py
@@ -0,0 +1,33 @@
+import os
+import json
+
+dir = os.path.abspath(__file__ + '/../')
+target = os.path.join(dir, 'annotated_6509.json')
+output = os.path.join(dir, 'annotated_6509.txt')
+
+with open(target) as inf, open(output, 'w') as of:
+  for line in inf:
+    item = eval(line)
+
+    of.write('fix:')
+    of.write('\n')
+    of.write(item['fix'])
+    of.write('\n')
+    of.write('\n')
+
+    of.write('bad:')
+    of.write('\n')
+    of.write(item['bad'])
+    of.write('\n')
+    of.write('\n')
+
+    of.write('annotated:')
+    of.write('\n')
+    of.write(item['annotated'])
+    of.write('\n')
+    of.write('\n')
+
+    of.write('\n')
+
+  inf.close()
+  of.close()
