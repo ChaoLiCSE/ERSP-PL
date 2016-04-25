881e4ba7d14a9881519dfeed1c2e7c9820ee267a
cs30xbi <cs30xbi@ieng6.ucsd.edu>
2016-04-24 23:32:22 -0700

    possible problem adding annotation

--- Spring 2016/compare_errs.py
+++ Spring 2016/compare_errs.py
@@ -11,17 +11,20 @@ def get_pos(prog):
     	m = re.search('(?<=Characters )([1-9][0-9][0-9]|[1-9][0-9]|[0-9])', error_output.stdout)
     	n = re.search('(?<=-)([1-9][0-9][0-9]|[1-9][0-9]|[0-9])', error_output.stdout)
     
+    '''
     print(m.group(0))
     print(n.group(0))
+    '''
     return[int(m.group(0)),int(n.group(0))]
 
 
 def err_judge(bad, fix, pos):
-	
+    
+    '''
     print (bad)
     print (fix)
     print (pos)
-
+    '''
 
 	#get the key word to judge whether it is changed
     to_judge = bad[pos[0]: pos[1]]
@@ -30,18 +33,19 @@ def err_judge(bad, fix, pos):
     s = SequenceMatcher(lambda x: x==" ", bad, fix)
     
     for tag, i1, i2, j1, j2 in s.get_opcodes():
-
+        '''
     	print ("%7s a[%d:%d] (%s) b[%d:%d] (%s)" % 
     		(tag, i1, i2, bad[i1:i2], j1, j2, fix[j1:j2]))
-
-    	#the code at that position has been changed
-    	if tag in ['replace' , 'delete'] and (i1 <= pos[0] or i2 >= pos[1]):
-    		print("correct location")
-    		return 1
+        '''
+         
+        #the code at that position has been changed
+        if tag in ['replace' , 'delete'] and (i1 <= pos[0] or i2 >= pos[1]):
+    		#print("correct location")
+            return 1
 
     	#the code at that position has not been changed
-    	if tag in ['equal'] and (i1 <= pos[0] and i2 >= pos[1]):
-            print ("incorrect location")
+        if tag in ['equal'] and (i1 <= pos[0] and i2 >= pos[1]):
+            #print ("incorrect location")
             return 0
 
     print ("cannot decide")
@@ -49,7 +53,7 @@ def err_judge(bad, fix, pos):
 
 
 dir = os.path.abspath(__file__ + '/../../')
-target = os.path.join(dir, 'list_of_errors.json')
+target = os.path.join(dir, 'annotated_6509.json')
 target2 = os.path.join(dir, 'problems.txt')
 '''
 prog = "let rec digitsOfInt n = match n with | ( n mod 10 ) + digitsOfInt ( (failwith "") 10 );;"
@@ -58,28 +62,72 @@ for i in get_pos (prog):
 '''
 total = 0
 prev_corr = 0
-add_corr = 0
+anno_corr = 0
 counter1 = 0
 
-with open (os.path.join(target), 'r') as myfile:
+both_correct = 0
+both_wrong = 0
+only_anno_corr = 0
+only_pre_corr = 0
+
+with open (os.path.join(target), 'r') as myfile, open (os.path.join(target2), 'w') as out:
     for line in myfile:
       
       #just test
+      '''
       if counter1 > 100:
         break
       counter1= counter1+1
+      '''
 
       item = eval(line)
-      for i in range(len(item["bad"])):
-        print(type(item["bad"][i]))
-        pos = get_pos(item["bad"][i])
+      #for i in range(len(item["bad"])):
+      #print(item["bad"])
+
+      pos = get_pos(item["bad"])
+      retVal = err_judge(item["bad"],item["fix"],pos)
         
-        retVal = err_judge(item["bad"][i],item["fix"][0],pos)
+
+      if(retVal == -1):
+          print('cannot judge at 1')
+          out.write('error at: ')
+          out.write(item[bad])
+          continue
+
+      total = total+1
+      if(retVal == 1):
+          prev_corr = prev_corr + 1
         
-        total = total+1
-        if(retVal == 1):
-           prev_corr = prev_corr + 1       
+      pos = get_pos(item["annotated"])
+      retVal_a = err_judge(item["annotated"], item["annotated_fix"], pos)
+
+      if(retVal == -1):
+          print('cannot judge at 2')
+          out.write('error at: ')
+          out.write(item[annotated])
+          continue
+
+      if(retVal_a == 1):
+          anno_corr = anno_corr+1
+
+      if(retVal_a == 1 and retVal == 1):
+            both_correct = both_correct +1
+      elif(retVal_a == 1 and retVal == 0):
+            only_anno_corr = only_anno_corr + 1
+            print('anno correct')
+            out.write('only_anno_corr')
+            out.write(line)
+            out.write('\n')
+      elif(retVal_a == 0 and retVal == 1):
+            only_pre_corr = only_pre_corr + 1
+            print('pre correct')
+            out.write('only_pre_corr')
+            out.write(line)
+            out.write('\n')
+      else:
+            both_wrong = both_wrong +1
 
 myfile.close() 
-s = 'in the total of ' + str(total) + ' prev_corr is: ' + str(prev_corr)
+s = 'in the total of ' + str(total) + ' programs, both correct is: ' + str(both_correct) + 'only annotated correct is: ' + str(only_anno_corr) + 'only pre correct is: ' + str(only_pre_corr) + 'both wrong is: '+str(both_wrong)
+
 print (s)
