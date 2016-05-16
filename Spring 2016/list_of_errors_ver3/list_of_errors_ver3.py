524542e651359b583e5f69e0113ca1a804367124
Yijun Zhang <Yijun Zhang>
2016-05-15 23:41:05 -0700

    generate new concise with corect min field

---
+++ Spring 2016/list_of_errors_ver3/list_of_errors_ver3.py
@@ -0,0 +1,140 @@
+import re
+import os
+import json
+import type_annotate
+
+"""
+updates from list_of_errors:
+1. use 'min' field as well as 'in'
+2. fix wierd pairing
+3. create timeline for each problem
+
+NOTE: used 'in' for fix but 'min' for bad!!
+"""
+
+dir = os.path.abspath(__file__ + '/../')
+target = os.path.join(dir, 'sp14-concise')
+output3 = os.path.join(dir, 'list_of_errors_ver2.json')
+
+problems_hw1 = ['palindrome', 'listReverse', 'digitalRoot', 'additivePersistence', 'digitsOfInt', 'sumList']
+problems_hw2 = ['build', 'eval', 'exprToString', 'expr', 'fixpoint', 'wwhile', 'removeDuplicates', 'assoc']
+problems_hw3 = ['bigMul', 'mulByDigit', 'bigAdd', 'removeZero', 'padZero', 'clone', 'stringOfList', 'sepConcat', 'pipe', 'sqsum']
+
+def find_problem_set(hw_num):
+  problem_set = list()
+
+  if hw_num == 'hw1':
+    problem_set = problems_hw1
+
+  elif hw_num == 'hw2':
+    problem_set = problems_hw2
+
+  elif hw_num == 'hw3':
+    problem_set = problems_hw3
+
+  return problem_set
+
+def find_all_prob(problem, lines):
+  prob_list = list()
+
+  for line in lines:
+    item = eval(line)
+    for j in item['ocaml']:
+      if problem in j['in']:
+        prob_list.append(item)
+        break
+      else:
+        continue
+      break
+
+  return prob_list
+
+def build_dict(hw_num, problem):
+  new_dict = dict()
+  new_dict['hw'] = hw_num
+  new_dict['problem'] = problem
+  new_dict['bad'] = []
+  new_dict['fix'] = []
+  new_dict['message'] = []
+  return new_dict
+
+### MAIN
+of3 = open(output3, 'a')
+count_bads = 0
+count_groups = 0
+count_no_fix = 0
+
+for i in os.listdir(target):
+
+  filename = str.split(i, '.')
+
+  student = filename[0]
+  hw_num = filename[1]
+
+  print(i)
+
+  with open(os.path.join(target, i)) as inf:
+
+    lines = inf.readlines()
+
+    problem_set = find_problem_set(hw_num)
+
+    for label in problem_set:
+      summary = build_dict(hw_num, label)
+      events = find_all_prob(label, lines)
+
+      index = 0
+
+      while index < len(events):
+
+        # find the first bad program
+        while index < len(events) and not events[index]['ocaml'][0]['min']:
+          index += 1
+          continue
+
+        while index < len(events):
+          # find all trailing bad programs until a fix
+          while index < len(events):
+
+            # bad programs
+            if events[index]['ocaml'][0]['out']:
+              summary['bad'].append(events[index]['ocaml'][0]['min'])
+              summary['message'].append(events[index]['ocaml'][0]['out'])
+              count_bads += 1
+              index += 1
+
+            # true fix, write to file
+            elif not type_annotate.annotate_and_compile(events[index], label, hw_num):
+              summary['fix'].append(events[index]['ocaml'][0]['min'])
+              index += 1
+
+              # write to file, ignore empty dicts
+              json.dump(summary, of3)
+              of3.write('\n')
+              count_groups += 1
+
+              break
+
+            # skip false fix
+            else:
+              continue
+
+          # reach end of the list but finds no fix
+          if index >= len(events) and not summary['fix']:
+            summary['fix'].append('')
+            count_no_fix += 1
+
+            # write to file
+            json.dump(summary, of3)
+            of3.write('\n')
+            count_groups += 1
+            break
+
+          # list is not end, continue
+          summary = build_dict(hw_num, label)
+
+
+of3.close()
+print(count_bads)
+print(count_no_fix)
+print(count_groups)
---
+++ Spring 2016/list_of_errors_ver3/type_annotate.py
@@ -0,0 +1,96 @@
+import os
+import re
+import json
+import subprocess
+
+hw1 = ['???','sumList','digitsOfInt', 'additivePersistence', 'digitalRoot', 'listReverse', 'palindrome']
+hw2 = ['???','assoc','removeDuplicates', 'wwhile','fixpoint', 'exprToString', 'eval', 'build']
+hw3 = ['???','sqsum', 'pipe', 'sepConcat', 'stringOfList', 'clone', 'padZero', 'removeZero', 'bigAdd', 'mulByDigit', 'bigMul']
+
+annotation_hw1 = ["",\
+  " : int list -> int", \
+  " : int -> int list", \
+  " : int -> int", \
+  " : int -> int", \
+  " : 'a list -> 'a list", \
+  " : string -> bool"]
+
+annotation_hw2 = ["",\
+  " : 'a * 'b * ('b * 'a) list -> 'a ", \
+  " : 'a list -> 'a list", \
+  " : ('a -> 'a * bool) * 'a -> 'a", \
+  ": ('a -> 'a) * 'a -> 'a", \
+  " : expr -> string", \
+  " : expr * float * float -> float", \
+  ": ((int * int -> int) * int) -> expr"]
+
+annotation_hw3 = ["",\
+  " : int list -> int ", \
+  " : ('a -> 'a) list -> ('a -> 'a)", \
+  " : string -> string list -> string", \
+  " : ('a -> string) -> 'a list -> string", \
+  " : 'a -> int -> 'a list", \
+  " : int list -> int list -> int list  * int list", \
+  " : int list -> int list", \
+  " : int list -> int list -> int list", \
+  " : int -> int list -> int list ", \
+  " : int list -> int list -> int list"]
+
+def add_annotation(annotation, problem_name, code):
+  # match the variables
+  var_regex = '(?<=' + problem_name + ')(.*?)(?=\=)'
+  var = re.search(var_regex, code)
+  # match the string to replace
+  my_regex = '(?<=' + problem_name + ')(.*?)\='
+  result = re.sub(my_regex, lambda match: replace(match, var.group(), annotation), code,1)
+  #print (result)
+  return result
+
+def replace(match, variables, annotation):
+  replacement = annotation
+  replacement += ' = fun '
+  replacement += variables
+  replacement += ' -> '
+  return replacement
+
+def find_annotation(indice):
+  annotation = ''
+  if indice['hw'] is 'hw1':
+    annotation = annotation_hw1[hw1.index(indice['problem'])]
+  elif indice['hw'] is 'hw2':
+    annotation = annotation_hw2[hw2.index(indice['problem'])]
+  elif indice['hw'] is 'hw3':
+    annotation = annotation_hw3[hw3.index(indice['problem'])]
+  return(annotation)
+
+def build_dict(bad, annotation, indice):
+  dic = dict()
+  dic['bad'] = bad
+  dic['annotated'] = add_annotation(annotation, indice['problem'], bad)
+  if not indice['fix']:
+    dic['fix'] = ''
+  else:
+    dic['fix'] = indice['fix'][0]
+  dic['annotated_fix'] = add_annotation(annotation, indice['problem'], dic['fix'])
+  return dic
+
+def find_annotation_with_label(hw_num, label):
+  annotation = ''
+  if hw_num is 'hw1':
+    annotation = annotation_hw1[hw1.index(label)]
+  elif hw_num is 'hw2':
+    annotation = annotation_hw2[hw2.index(label)]
+  elif hw_num is 'hw3':
+    annotation = annotation_hw3[hw3.index(label)]
+  return(annotation)
+
+def annotate_and_compile(indice, label, hw_num):
+  annotation = find_annotation_with_label(indice, hw_num)
+  annotated_prog = add_annotation(annotation, label, indice['ocaml'][0]['min'])
+  error_output = subprocess.run(["ocaml"], input = indice['ocaml'][0]['min'], 
+                                stdout=subprocess.PIPE,universal_newlines = True)
+  if "rror" in error_output.stdout:
+    print(error_output.stdout)
+    return error_output.stdout
+  else:
+    return ""
